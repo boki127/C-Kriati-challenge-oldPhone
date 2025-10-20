c31bf24dadec81d7947f45125efec5675cb94835
boki127 <kiratizeza@gmail.com>
2025-10-21 04:01:39 +0700

    ver... validate

--- Kirati_challenge_oldPhone/Program.cs
+++ Kirati_challenge_oldPhone/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Diagnostics.Metrics;
 using System.Runtime.ConstrainedExecution;
@@ -18,34 +19,13 @@ namespace MyApp
 
             while (playAgain)
             {
-                // init params default value
-                isValid = false;
-                // validate user input
-                do {
-                    // recieve user input
-                    Console.Write("Input your OldPhonePad : ");
-                    userInput = Console.ReadLine();
-
-                    // case1 : valid normal input
-                    if (Regex.IsMatch(userInput, @"^[0-9* ]+#$")) {
-                        //Console.WriteLine("valid");
-                        isValid = true;
-                    } 
-                    // case2 : valid blank input
-                    else if (userInput == "#") {
-                        //Console.WriteLine("valid");
-                        isValid = true;
-                    }
-                    // case3 : the input not valid go into loop
-                    if (!isValid)
-                    {
-                        Console.WriteLine("you problem is not valid");
-                    }
-                } while (!isValid);
+                // simple input reciever
+                Console.Write("Input your OldPhonePad : ");
+                userInput = Console.ReadLine();
 
                 // decription the word
                 displayAnswer = OldPhone.OldPhonePad(userInput);
-                Console.WriteLine("Your actual word is : " + displayAnswer);
+                Console.WriteLine("Output : " + displayAnswer);
 
                 // end condition to exit the program
                 Console.Write("Do you want to exit (Y/N): ");
@@ -59,7 +39,6 @@ namespace MyApp
             }
 
         }
-
         
     }
     public static class OldPhone
@@ -69,93 +48,100 @@ namespace MyApp
             string problem = input;
             int counter = 0;
             Stack<char> ansStack = new Stack<char>();
-            char currText;
             string result;
 
+            // case1 : valid normal input
+            if (Regex.IsMatch(problem, @"^[0-9* ]+#$"))
+            {
+            }
+            // case2 : valid blank input
+            else if (problem == "#")
+            {
+                return "";
+            }
+            // case3 : the input not valid go into loop
+            else {
+                return "you input is not valid";
+            }
 
             for (int i = 0; i < problem.Length - 1; i++)
             {
+                // counting-part
                 if (problem[i] == problem[i + 1])
                 {
                     // increae counter to count the dup
                     counter++;
 
-                    // '*' case Pop() 
-                    if (problem[i] == '*' && ansStack.Count > 0)
+                    // '*' and '0' case : do nothing
+                    if (problem[i] == '*' || problem[i] == '0')
                     {
-                        ansStack.Pop();
-                        counter = 0;
+                        //do nothing 
                     }
-                    // '0' case push space
-                    else if (problem[i] == '0')
-                    {
-                        ansStack.Push(' ');
-                        counter = 0;
-                    }
-                    // ' ' case donothing and reset counter
+                    // ' ' case do nothing and reset counter
                     else if (problem[i] == ' ')
                     {
                         counter = 0;
                     }
+                    // have 4 looop case : 1, 2, 3, 4, 1(5), 2(6), 3(7)
                     else if (problem[i] is '7' or '9')
                     {
                         counter %= 4;
                     }
+                    // have 3 looop case : 1, 2, 3, 1(4), 2(5), 3(6)
                     else
                     {
                         counter %= 3;
                     }
                 }
+                // pushing-poping-part
                 else if (problem[i] != problem[i + 1])
                 {
                     switch (problem[i])
                     {
                         case '*':
-                            if (ansStack.Count > 0)
+                         
+                            for (int j = 0; j < counter+1; j++)
                             {
-                                ansStack.Pop();
-                                //Console.WriteLine("Poping ");
+                                if (ansStack.Count > 0)
+                                {
+                                    ansStack.Pop();
+                                }
                             }
                             break;
                         case '0':
-                            ansStack.Push(' ');
+                            for (int j = 0; j < counter+1; j++)
+                            {
+                                ansStack.Push(' ');
+                            }
                             break;
                         case ' ':
                             //do nothing
                             break;
                         case '1':
-                            currText = (char)(38 + counter);
-                            ansStack.Push(currText);
-                            //Console.WriteLine("push " + currText);
+                            ansStack.Push((char)(38 + counter));
                             break;
                         case '7':
-                            currText = (char)(80 + counter);
-                            ansStack.Push(currText);
-                            //Console.WriteLine("push " + currText);
+                            ansStack.Push((char)(80 + counter));
                             break;
                         case '8':
-                            currText = (char)(84 + counter);
-                            ansStack.Push(currText);
-                            //Console.WriteLine("push " + currText);
+                            ansStack.Push((char)(84 + counter));
                             break;
                         case '9':
-                            currText = (char)(87 + counter);
-                            ansStack.Push(currText);
-                            Console.WriteLine("push " + currText);
+                            ansStack.Push((char)(87 + counter));
                             break;
                         default:
                             // basecase = a + ((char * 3) + counter )
-                            currText = (char)(65 + ((Convert.ToInt32(problem[i]) - 50) * 3) + counter);
-                            ansStack.Push(currText);
-                            //Console.WriteLine("push " + currText);
+                            ansStack.Push((char)(65 + ((Convert.ToInt32(problem[i]) - 50) * 3) + counter));
                             break;
                     }
+                    // reset-counter
                     counter = 0;
 
                 }
 
             }
 
+            // prep result
             char[] chararray = ansStack.ToArray();
             Array.Reverse(chararray);
             result = string.Join("", chararray);
